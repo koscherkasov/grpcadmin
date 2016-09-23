cedc2ceb3f935ee556eddb80718881d568454a6e
koscherkasov <koscher91@gmail.com>
2016-09-23 22:27:21 +0400

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='grpcadmin',
+      version='0.1',
+      description='Admin for hrpc',
+      url='https://github.com/koscherkasov/grpcadmin',
+      author='',
+      author_email='john@example.com',
+      license='MIT',
+      packages=['grpcadmin'],
+      zip_safe=False,
+      scripts=['bin/joke'])
