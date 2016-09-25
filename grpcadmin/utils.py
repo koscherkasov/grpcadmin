8fe70e070552ebf3351787b3ba7b5b92f68e9650
koscherkasov <koscher91@gmail.com>
2016-09-25 22:08:14 +0400

    add dir and files creation

--- grpcadmin/utils.py
+++ grpcadmin/utils.py
@@ -1,6 +1,22 @@
-def opa(a):
-    return a + a + a
+import os
+
+ENCODING = 'utf-8'
+PROTO_BUF_DIR = 'proto_buf'
+PROTO_PY_DIR = 'proto_py'
+SERVICES_DIR = 'services'
+INIT_FILE = '__init__.py'
+PROTO_FORMAT = '.proto'
+PROTO_TEMPLATE = 'syntax="proto2"\n'
 
 
 def create_service(name):
-    print(name)
+    os.makedirs(name, exist_ok=False)
+    os.makedirs(os.path.join(name, PROTO_BUF_DIR))
+    with open(os.path.join(name, PROTO_BUF_DIR, name + PROTO_FORMAT), 'tw', encoding=ENCODING) as f:
+        f.write(PROTO_TEMPLATE)
+
+    os.makedirs(os.path.join(name, PROTO_PY_DIR))
+    open(os.path.join(name, PROTO_PY_DIR, INIT_FILE), 'tw').close()
+
+    os.makedirs(os.path.join(name, SERVICES_DIR))
+    open(os.path.join(name, SERVICES_DIR, INIT_FILE), 'tw').close()
