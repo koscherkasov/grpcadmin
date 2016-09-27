4e9b9b2a1d33e2ffdd7f728e809331740f6ae5e1
koscherkasov <koscher91@gmail.com>
2016-09-27 15:08:14 +0400

    add compile

--- grpcadmin/scripts.py
+++ grpcadmin/scripts.py
@@ -1,5 +1,5 @@
 import click
-from grpcadmin.utils import create_service
+from grpcadmin.utils import create_service, compile_services
 
 
 @click.group()
@@ -21,31 +21,42 @@ def create(name):
 
 
 @cli.command()
-@click.option('--name', '-n', multiple=True, type=click.STRING,
+@click.option('--include', '-i', multiple=True, type=click.STRING,
               help="Name of service will be compiled. It's multiple option. "
                    "If the option is missed all services will be compiled.")
-@click.option('--except_for', '-e', multiple=True, type=click.STRING,
+@click.option('--exclude', '-e', multiple=True, type=click.STRING,
               help="Name of service won't be compiled. It's multiple option.")
-def compile(name, except_for):
+def compile(include, exclude):
     """Service for compile proto files to python.
-    Service get proto files from directory: <service_name>/proto_buf/ @TODO
-
-    For compilation all proto files @TODO
+    Service compile *.py files by proto files from directory: /proto_buf/<service_name>.proto
+    You can explicitly include necessary files for compilation,
+    otherwise grpc-admin will compile all proto files from proto_buf directory.
 
     \b
-    Example:
-        $grpc-admin create example.
-        This command create dirs for service "example" with stub proto file for future filling"""
-    click.echo((name, except_for))
+    Example1:
+        $grpc-admin compile
+        This command will compile all proto file from proto_buf directory
+
+    \b
+    Example2:
+        $grpc-admin compile -e service1
+        This command will compile all proto file from proto_buf directory except for service1
+
+    \b
+    Example3:
+        $grpc-admin compile -i service1 -i service2
+        This command will compile only two services: service1 and service2"""
+
+    compile_services(include, exclude)
 
 
 @cli.command()
-@click.option('--adress', '-a', default='', type=click.STRING, help='adress')
+@click.option('--address', '-a', default='', type=click.STRING, help='address')
 @click.option('--max_workers', '-m', default=1, type=click.IntRange(min=1, max=128), help='number of workers')
 @click.option('--protofile', '-p', multiple=True,
               type=click.STRING, help="Proto file. It's multiple option.")
-def server(adress, max_workers, protofile):
+def server(address, max_workers, protofile):
     """comment"""
-    click.echo(adress)
+    click.echo(address)
     click.echo(max_workers)
     click.echo(protofile)
--- grpcadmin/utils.py
+++ grpcadmin/utils.py
@@ -1,4 +1,5 @@
 import os
+from grpc.tools import protoc
 
 ENCODING = 'utf-8'
 PROTO_BUF_DIR = 'proto_buf'
@@ -6,7 +7,7 @@ PROTO_PY_DIR = 'proto_py'
 SERVICES_DIR = 'services'
 INIT_FILE = '__init__.py'
 PROTO_FORMAT = '.proto'
-PROTO_TEMPLATE = 'syntax="proto2"\n'
+PROTO_TEMPLATE = 'syntax="proto2";\n'
 
 
 def create_service(name):
@@ -31,3 +32,59 @@ def create_service(name):
     # create package services
     os.makedirs(os.path.join(name, SERVICES_DIR))
     open(os.path.join(name, SERVICES_DIR, INIT_FILE), 'w').close()
+
+
+def run_code_generator(services):
+    for name in services:
+        protoc.main((
+            '',
+            '-I./proto_buf',
+            '--python_out=./proto_py',
+            '--grpc_python_out=./proto_py',
+            './proto_buf/{}.proto'.format(name),
+        ))
+
+
+def compile_services(include, exclude):
+    """Compile proto files to python.
+    If include argument is empty all services will be compiled
+
+    :param include: names of the services will be included
+    :type include: tuple
+    :param exclude: names of the services won't be included
+    :type exclude: tuple
+    """
+
+    # check where i am
+    if not os.path.isdir(PROTO_BUF_DIR):
+        raise FileNotFoundError("Directory {} not found".format(PROTO_BUF_DIR))
+    if not os.path.isdir(PROTO_PY_DIR):
+        raise FileNotFoundError("Directory {} not found".format(PROTO_PY_DIR))
+    if not os.path.isdir(SERVICES_DIR):
+        raise FileNotFoundError("Directory {} not found".format(SERVICES_DIR))
+
+    # get all proto files from proto_buf dir
+    proto_files = [f for f in os.listdir(PROTO_BUF_DIR)
+                   if os.path.isfile(os.path.join(PROTO_BUF_DIR, f)) and f.endswith('.proto')]
+    all_services = [os.path.splitext(f)[0] for f in proto_files]
+    if not all_services:
+        raise FileNotFoundError("Directory {} hasn't got any proto files".format(PROTO_BUF_DIR))
+
+    # get necessary service names
+    services = []
+    for include_service in include:
+        if include_service not in all_services:
+            raise FileNotFoundError("Service can't be included because file {}.proto is already "
+                                    "missing in {} directory".format(include_service, PROTO_BUF_DIR))
+        services.append(include_service)
+    services = services or all_services
+
+    for exclude_service in exclude:
+        try:
+            services.remove(exclude_service)
+        except ValueError:
+            print("Service {} can't be excluded".format(exclude_service))
+
+    # compile
+    run_code_generator(services)
+
