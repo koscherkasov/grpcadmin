cbffde543b0e7e41374fe7b76eca1eb3c8fe66d2
koscherkasov <koscher91@gmail.com>
2016-09-27 19:05:14 +0400

    add service

--- grpcadmin/scripts.py
+++ grpcadmin/scripts.py
@@ -1,5 +1,5 @@
 import click
-from grpcadmin.utils import create_service, compile_services
+from grpcadmin.utils import create_service, compile_services, add_service_stub
 
 
 @click.group()
@@ -27,8 +27,8 @@ def create(name):
 @click.option('--exclude', '-e', multiple=True, type=click.STRING,
               help="Name of service won't be compiled. It's multiple option.")
 def compile(include, exclude):
-    """Service for compile proto files to python.
-    Service compile *.py files by proto files from directory: /proto_buf/<service_name>.proto
+    """Compile proto files to python.
+    The command compiles *.py files by proto files from directory: /proto_buf/<service_name>.proto
     You can explicitly include necessary files for compilation,
     otherwise grpc-admin will compile all proto files from proto_buf directory.
 
@@ -60,3 +60,32 @@ def server(address, max_workers, protofile):
     click.echo(address)
     click.echo(max_workers)
     click.echo(protofile)
+
+
+@cli.command()
+@click.option('--include', '-i', multiple=True, type=click.STRING,
+              help="Name of service will be compiled. It's multiple option. "
+                   "If the option is missed all services will be compiled.")
+@click.option('--exclude', '-e', multiple=True, type=click.STRING,
+              help="Name of service won't be compiled. It's multiple option.")
+def service(include, exclude):
+    """Add stubs for services
+    You can explicitly include necessary services,
+    otherwise grpc-admin will create stubs for all services in proto_buf directory.
+
+    \b
+    Example1:
+        $grpc-admin service
+        This command  will create stubs for all services in proto_buf directory.
+
+    \b
+    Example2:
+        $grpc-admin service -e service1
+        This command will create stubs for all services in proto_buf directory except for service1
+
+    \b
+    Example3:
+        $grpc-admin service -i service1 -i service2
+        This command will compile only two services: service1 and service2"""
+
+    add_service_stub(include, exclude)
--- grpcadmin/utils.py
+++ grpcadmin/utils.py
@@ -35,6 +35,12 @@ def create_service(name):
 
 
 def run_code_generator(services):
+    """Compile services to python from proto files
+
+    :param services: names of the services
+    :type services: list of str
+    :return:
+    """
     for name in services:
         protoc.main((
             '',
@@ -45,9 +51,18 @@ def run_code_generator(services):
         ))
 
 
+def check_where_am_i():
+    if not os.path.isdir(PROTO_BUF_DIR):
+        raise FileNotFoundError("Directory {} not found".format(PROTO_BUF_DIR))
+    if not os.path.isdir(PROTO_PY_DIR):
+        raise FileNotFoundError("Directory {} not found".format(PROTO_PY_DIR))
+    if not os.path.isdir(SERVICES_DIR):
+        raise FileNotFoundError("Directory {} not found".format(SERVICES_DIR))
+
+
 def compile_services(include, exclude):
     """Compile proto files to python.
-    If include argument is empty all services will be compiled
+    If "include" argument is empty all services will be compiled
 
     :param include: names of the services will be included
     :type include: tuple
@@ -55,13 +70,8 @@ def compile_services(include, exclude):
     :type exclude: tuple
     """
 
-    # check where i am
-    if not os.path.isdir(PROTO_BUF_DIR):
-        raise FileNotFoundError("Directory {} not found".format(PROTO_BUF_DIR))
-    if not os.path.isdir(PROTO_PY_DIR):
-        raise FileNotFoundError("Directory {} not found".format(PROTO_PY_DIR))
-    if not os.path.isdir(SERVICES_DIR):
-        raise FileNotFoundError("Directory {} not found".format(SERVICES_DIR))
+    # check where am I
+    check_where_am_i()
 
     # get all proto files from proto_buf dir
     proto_files = [f for f in os.listdir(PROTO_BUF_DIR)
@@ -88,3 +98,24 @@ def compile_services(include, exclude):
     # compile
     run_code_generator(services)
 
+
+def add_service_stub(include, exclude):
+    """@TODO
+
+    :param include: names of the services which stubs will be created
+    :type include: tuple
+    :param exclude: names of the services which stubs won't be created
+    :type exclude: tuple
+    """
+    # check where am I
+    check_where_am_i()
+
+    current_dir = os.getcwd()
+    service_name = os.path.split(current_dir)[1]
+    service_py_file = os.path.join(current_dir, SERVICES_DIR, service_name+'.py')
+    service_exist = os.path.isfile(service_py_file)
+    print(include)
+    print(exclude)
+    print(service_exist)
+    if not service_exist:
+        open(service_py_file, 'w').close()
